387e35009d4adaf2872864b8e9e1f5cc28a95dfe
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-02-18 16:15:46 -0500

    ClienteActivity Updated with Activity_cliente.xml information

--- app/src/main/java/com/example/concesionario_sabado/ClienteActivity.java
+++ app/src/main/java/com/example/concesionario_sabado/ClienteActivity.java
@@ -3,12 +3,27 @@ package com.example.concesionario_sabado;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class ClienteActivity extends AppCompatActivity {
+
+    EditText jetidentificacion, jetnombre, jetcorreo;
+    CheckBox jcbactivo;
+
+    String identificacion, nombre, correo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente);
+        //Ocultar la barra de titulo por defecto y voy a asociar objetos Java con XML
+        getSupportActionBar().hide();
+        jetidentificacion = findViewById(R.id.etidentificacion);
+        jetnombre = findViewById(R.id.etnombre);
+        jetcorreo = findViewById(R.id.etcorreo);
+        jcbactivo = findViewById(R.id.cbactivo);
+
     }
 }
